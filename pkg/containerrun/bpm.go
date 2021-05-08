f48b916c2000e07578086224d4ff7080742aa309
Jan Dubois <jan.dubois@suse.com>
2021-05-08 12:09:15 -0700

    Add `bpm term` command; same as stop, but don't wait
    Also modify `bpm running` to only print yes/no when stdout is a tty.
    
    Signed-off-by: Jan Dubois <jan.dubois@suse.com>

--- pkg/containerrun/bpm.go
+++ pkg/containerrun/bpm.go
@@ -13,14 +13,16 @@ func WriteBPMscript() error {
 	script := `#!/bin/bash
 
 function usage {
-    echo "usage: $0 [start|stop|quit|running] JOBNAME [-p PROCESSNAME]"
+    echo "usage: $0 [start|stop|quit|term|running] JOBNAME [-p PROCESSNAME]"
     exit 1
 }
 
 if [ $# != 2 -a $# != 4 ]; then
     usage
 fi
-if [ "$1" != "start" -a "$1" != "stop" -a "$1" != "quit" -a "$1" != "running" ]; then
+if [ "$1" != "start" -a "$1" != "stop" -a "$1" != "running" -a \
+     "$1" != "quit" -a "$1" != "term" ]
+then
     usage
 fi
 
@@ -37,15 +39,18 @@ fi
 
 CONTAINER_RUN="/var/vcap/data/${JOB}/${PROCESS}_containerrun"
 if [ "$CMD" == "running" ]; then
+    # Print yes/no if stdout is a tty
     if [ -f "${CONTAINER_RUN}.running" ]; then
-        echo "yes"
+        test -t 1 && echo "yes"
         exit 0
     else
-        echo "no"
+        test -t 1 && echo "no"
         exit 1
     fi
 else
-    echo "${CMD^^}" | nc -w 1 -uU "${CONTAINER_RUN}.sock"
+    # "term" is the same as "stop", except we won't wait
+    ACTION="${CMD/term/stop}"
+    echo "${ACTION^^}" | nc -w 1 -uU "${CONTAINER_RUN}.sock"
     if [ "${CMD}" == "stop" ]; then
         for i in $(seq 30); do
             test ! -f "${CONTAINER_RUN}.running" && exit 0
